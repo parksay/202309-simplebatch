3b0ff4a3841407563075fdfaaa5d890e0d88c019
parksay_laptop <hellobigworld.parksay@gmail.com>
2023-09-04 09:39:37 +0900

    initial commit

---
+++ src/main/java/com/learning/simplebatch/SimplebatchApplication.java
@@ -0,0 +1,13 @@
+package com.learning.simplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/simplebatch/SimplebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.simplebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
